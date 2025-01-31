e2e2114e2c29141a4d914809e367ac5946728e97
itagagaki <itagagaki@gmail.com>
2025-01-31 15:53:41 +0900

    Fix to get icon

--- GoToWindow.Api/WindowEntryFactory.cs
+++ GoToWindow.Api/WindowEntryFactory.cs
@@ -4,54 +4,48 @@ using System.Text;
 
 namespace GoToWindow.Api
 {
-	public static class WindowEntryFactory
-	{
-		[DllImport("user32.dll")]
-		private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+    public static class WindowEntryFactory
+    {
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
 
-		[DllImport("user32.dll")]
-		private static extern int GetWindowTextLength(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
 
-		[DllImport("user32.dll", SetLastError = true)]
-		private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
 
-		[DllImport("user32.dll")]
-		[return: MarshalAs(UnmanagedType.Bool)]
-		private static extern bool IsIconic(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsIconic(IntPtr hWnd);
 
-		public static WindowEntry Create(IntPtr hWnd)
-		{
-			GetWindowThreadProcessId(hWnd, out uint processId);
+        public static WindowEntry Create(IntPtr hWnd)
+        {
+            GetWindowThreadProcessId(hWnd, out uint processId);
+            return Create(hWnd, processId);
+        }
 
-			return Create(hWnd, processId);
-		}
+        public static WindowEntry Create(IntPtr hWnd, uint processId)
+        {
+            return new WindowEntry
+            {
+                HWnd = hWnd,
+                ProcessId = processId,
+                Title = GetWindowTitle(hWnd),
+                IconHandle = WindowIcon.GetIcon(hWnd),
+                IsVisible = !IsIconic(hWnd)
+            };
+        }
 
-		public static WindowEntry Create(IntPtr hWnd, uint processId)
-		{
-			var windowTitle = GetWindowTitle(hWnd);
+        private static string GetWindowTitle(IntPtr hWnd)
+        {
+            var lLength = GetWindowTextLength(hWnd);
+            if (lLength == 0)
+                return null;
 
-			var iconHandle = WindowIcon.GetAppIcon(hWnd);
-			var isVisible = !IsIconic(hWnd);
-
-			return new WindowEntry
-			{
-				HWnd = hWnd,
-				Title = windowTitle,
-				ProcessId = processId,
-				IconHandle = iconHandle,
-				IsVisible = isVisible
-			};
-		}
-
-		private static string GetWindowTitle(IntPtr hWnd)
-		{
-			var lLength = GetWindowTextLength(hWnd);
-			if (lLength == 0)
-				return null;
-
-			var builder = new StringBuilder(lLength);
-			GetWindowText(hWnd, builder, lLength + 1);
-			return builder.ToString();
-		}
-	}
+            var builder = new StringBuilder(lLength);
+            GetWindowText(hWnd, builder, lLength + 1);
+            return builder.ToString();
+        }
+    }
 }
--- GoToWindow.Api/WindowIcon.cs
+++ GoToWindow.Api/WindowIcon.cs
@@ -3,35 +3,55 @@ using System.Runtime.InteropServices;
 
 namespace GoToWindow.Api
 {
-	public static class WindowIcon
-	{
-		public const int PerIconTimeoutMilliseconds = 50;
+    public static class WindowIcon
+    {
+        public const int PerIconTimeoutMilliseconds = 50;
 
-		// ReSharper disable InconsistentNaming
-		public const int ICON_SMALL = 0;
-		public const int ICON_BIG = 1;
-		public const int ICON_SMALL2 = 2;
+        // ReSharper disable InconsistentNaming
+        public const int ICON_SMALL = 0;
+        public const int ICON_BIG = 1;
+        public const int ICON_SMALL2 = 2;
+        public const int GCL_HICONSM = -34;
+        public const int GCL_HICON = -14;
 
-		public const int WM_GETICON = 0x7F;
+        public const int WM_GETICON = 0x7F;
 
-		public const int SMTO_ABORTIFHUNG = 0x0002;
-		// ReSharper restore InconsistentNaming
+        public const int SMTO_ABORTIFHUNG = 0x0002;
+        // ReSharper restore InconsistentNaming
 
-		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
-		private static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint  msg, uint  wParam, int lParam, uint  fuFlags, uint  uTimeout, out IntPtr lpdwResult);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        private static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint  msg, uint  wParam, int lParam, uint  fuFlags, uint  uTimeout, out IntPtr lpdwResult);
 
-		public static IntPtr GetAppIcon(IntPtr hwnd)
-		{
-			// http://codeutopia.net/blog/2007/12/18/find-an-applications-icon-with-winapi/
-			var result = SendMessageTimeout(hwnd, WM_GETICON, ICON_BIG, 0, SMTO_ABORTIFHUNG, PerIconTimeoutMilliseconds, out IntPtr iconHandle);
-			if (result != IntPtr.Zero && iconHandle == IntPtr.Zero)
-				result = SendMessageTimeout(hwnd, WM_GETICON, ICON_SMALL, 0, SMTO_ABORTIFHUNG, PerIconTimeoutMilliseconds, out iconHandle);
-			if (result != IntPtr.Zero && iconHandle == IntPtr.Zero)
-				result = SendMessageTimeout(hwnd, WM_GETICON, ICON_SMALL2, 0, SMTO_ABORTIFHUNG, PerIconTimeoutMilliseconds, out iconHandle);
-			if(result != IntPtr.Zero)
-				return iconHandle;
+        [DllImport("user32.dll", EntryPoint = "GetClassLong")]
+        public static extern uint GetClassLongPtr32(IntPtr hWnd, int nIndex);
 
-			return IntPtr.Zero;
-		}
-	}
+        [DllImport("user32.dll", EntryPoint = "GetClassLongPtr")]
+        public static extern IntPtr GetClassLongPtr64(IntPtr hWnd, int nIndex);
+
+        public static IntPtr GetClassLongPtr(IntPtr hWnd, int nIndex)
+        {
+            if (IntPtr.Size > 4)
+                return GetClassLongPtr64(hWnd, nIndex);
+            else
+                return new IntPtr(GetClassLongPtr32(hWnd, nIndex));
+        }
+
+        public static IntPtr GetIcon(IntPtr hwnd)
+        {
+            IntPtr iconHandle = IntPtr.Zero;
+
+            var result = SendMessageTimeout(hwnd, WM_GETICON, ICON_BIG, 0, SMTO_ABORTIFHUNG, PerIconTimeoutMilliseconds, out iconHandle);
+            if (result != IntPtr.Zero && iconHandle == IntPtr.Zero)
+                result = SendMessageTimeout(hwnd, WM_GETICON, ICON_SMALL, 0, SMTO_ABORTIFHUNG, PerIconTimeoutMilliseconds, out iconHandle);
+            if (result != IntPtr.Zero && iconHandle == IntPtr.Zero)
+                result = SendMessageTimeout(hwnd, WM_GETICON, ICON_SMALL2, 0, SMTO_ABORTIFHUNG, PerIconTimeoutMilliseconds, out iconHandle);
+            if (result != IntPtr.Zero && iconHandle != IntPtr.Zero)
+                return iconHandle;
+
+            iconHandle = GetClassLongPtr(hwnd, GCL_HICON);
+            if (iconHandle == IntPtr.Zero)
+                iconHandle = GetClassLongPtr(hwnd, GCL_HICONSM);
+            return iconHandle;
+        }
+    }
 }
